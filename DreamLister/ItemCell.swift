780b0b11b13621a0db648a8e2d516647dadd5378
Bojan Vujnovic <bojan.vujn@yahoo.com>
2017-02-18 17:18:46 +0100

    MaterialView extension

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Mac on 2/18/17.
+//  Copyright © 2017 Boki. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
